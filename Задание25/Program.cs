175b08f712150bb51ad6bdb8976aedd19b068c5b
Ваше имя английскими буквами <>
2024-05-21 11:46:18 +0500

    Initial commit

---
+++ Задание25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+using System;
+Console.Clear();
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите натуральное число B: ");
+int B = int.Parse(Console.ReadLine());
+
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+    result *= A;
+}
+Console.WriteLine($"{A} в степени {B} равно {result}");
---
+++ Задача29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+using System;
+Console.Clear();
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = i + 1; //
+}
+
+Console.WriteLine("Элементы массива:");
+foreach (int element in array)
+{
+    Console.Write(element + " ");
+}
